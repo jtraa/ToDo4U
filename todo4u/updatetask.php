9033ae6767b7fb08c793c9999a8586182ead0955
Jelle Traa <jelletraa@gmail.com>
2019-03-22 11:48:30 +0100

    edit updatetask errorproof plus old task items are displayed in the form

--- todo4u/updatetask.php
+++ todo4u/updatetask.php
@@ -1,8 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>TODO4U</title>
+    <link rel="icon" href="img/post-it.png"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+</head>
+<body>
+
 <?php
-
-
-
-
 include 'dbconnection.php';
 
 session_start();
@@ -19,26 +27,30 @@ $db_result = $conn->query($sql_querie);
 foreach ($db_result as $row)
 {$_SESSION['id']=$row['id'];
 
-echo '<form autocomplete="off" method="POST" action="updatehandler.php">' .
-        
+echo '<center>' . '<form autocomplete="off" method="POST" action="updatehandler.php">' .
+        '<div class="heading">' .
+        '<center> <h1>' . 'Update Task' . '</h1> </center>' .
+        '</div>' .  
         '<br><br>' .
         "Title" . '<br>' .
-        '<input type="text" name="task" class="task_input" placeholder=' . $row['task'] . 'required>' . 
+        '<input type="text" name="task" class="task_input" value="' . $row['task'] . '" required>' . 
         '<br><br>' .
         'Note <br>' .
-        '<textarea rows="4" cols="50" name="note" class="task_input" placeholder=' . $row['note'] . '>' . '</textarea>' .
+        '<textarea rows="4" cols="50" name="note" class="task_input">' . $row['note'] . '</textarea>' .
         '<br><br>'.
         'Begin <br>'.
-        '<input type="date" name="begindate" min="2015-01-01" value=' . $row['begindate'] . 'required>' .
+        '<input type="date" name="begindate" min="2015-01-01" value="' . $row['begindate'] . '" required>' .
         '<br>' .
-        'Completed <br>'.
-        '<input type="date" name="date" min="2015-01-01" value=' . $row['date'] . 'required><br><br>' .
+        'Completed <br>' .
+        '<input type="date" name="date" min="2015-01-01" value="' . $row['date'] .  '" required><br><br>' .
 
         
 
         '<button type="submit" name="submit" class="task_btn">Save</button>' .
     
-    '</form>';
+    '</form>' . '</center>';
 }
 
 ?>
+</body>
+</html>
