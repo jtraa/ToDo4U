d7f538c2435271005268862e4f195a58734e19fb
Jelle Traa <jelletraa@gmail.com>
2019-04-04 09:50:34 +0200

    small javascript element for deleting product

--- todo4u/todo4u W3/update_task.php
+++ todo4u/todo4u W3/update_task.php
@@ -52,7 +52,7 @@ $page_title = "Update Task";
 include_once "layout_header.php";
 
 echo "<div class='right-button-margin'>";
-    echo "<a href='index.php' class='btn btn-default pull-right'>Read Tasks</a>";
+    echo "<a href='index.php' class='btn btn-default pull-right'>Show Tasks</a>";
 echo "</div>";
  
 
